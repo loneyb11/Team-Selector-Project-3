ed29306c9a316dd51abb39a68b2e7e6fbe7db02d
loneyb11 <59574123+loneyb11@users.noreply.github.com>
2020-06-23 13:00:26 -0500

    Added React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Team Se11ector</h1>
+      <h2>Pick your team and share it!</h2>
+    </div>
+  );
+}
